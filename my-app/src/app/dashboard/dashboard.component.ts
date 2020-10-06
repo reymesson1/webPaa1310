4e5a37ae131ba13dc95ed0f4cfec65deb4056375
reymesson1 <reymesson1>
2020-10-06 01:40:53 -0400

    done

--- my-app/src/app/dashboard/dashboard.component.ts
+++ my-app/src/app/dashboard/dashboard.component.ts
@@ -32,7 +32,9 @@ export class DashboardComponent implements OnInit {
   dialogRef
   dialogRefLoading
   filterStatus = '';
-
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions = [5, 10, 25, 100];
 
   ngOnInit(): void { 
 
